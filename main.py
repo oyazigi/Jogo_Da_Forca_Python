f814ea708da4266547cca0886559e5f5fa51b783
yazigi <mambo0001@outlook.com>
2022-02-04 21:15:54 -0300

    Adicionando leitura em bloco de notas e adicionando desenho da forca quando o jogador erra uma letra

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 import random
-resposta = ['carro', 'passear', 'cachorro', 'mouse', 'celular', 'telefone', 'liquidificador', 'mexerica', 'cidade', 'cagar']
+resposta = carregar_palavra()
 def gerar_palavra():
     global palavra
     palavra = random.choice(resposta)
     return palavra
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 def posicaoletra(string, char):
     pos = []
@@ -12,6 +61,20 @@ def posicaoletra(string, char):
             pos.append(n+1)
     return pos
 
+def carregar_palavra():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+
+        palavras.append(linha)
+
+    arquivo.close()
+
+    palavra_secreta = random.choice(palavras).upper()
+    return palavra_secreta
+
 print("\n*********************************")
 print("Bem vindo ao jogo da Forca!")
 print("*********************************\n")
@@ -19,8 +82,8 @@ print("A Sua palavra contém" , len(gerar_palavra()) , "letras")
 
 letras_restantes = len(palavra)
 letras_chutadas = []
-tentativas = 5
-while tentativas > 0:
+erros = 0
+while erros != 7:
 
     chute = input("Digite o seu chute ")
     if (letras_chutadas.count(chute) > 0):
@@ -31,7 +94,8 @@ while tentativas > 0:
     posicao = palavra.find(chute)
     if(posicao == -1):
         print("A palavra não contém essa letra\n")
-        tentativas = tentativas - 1
+        erros += 1
+        desenha_forca(erros)
     else:
        print("\nvocê chutou certo! essa letra esta na posição: {}".format(posicaoletra(palavra, chute)))
        letras_restantes = letras_restantes - len(posicaoletra(palavra, chute))
